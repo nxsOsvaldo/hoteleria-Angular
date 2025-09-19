c2407984816e50d1e09fa9aa72a036f76a1c3f82
Osvaldo Deloya <deloyaosvaldo@gmail.com>
2025-09-19 10:07:02 -0600

    Enrutamiento

--- hoteleria-Angular/src/app/app-routing.module.ts
+++ hoteleria-Angular/src/app/app-routing.module.ts
@@ -9,18 +9,41 @@ import { HabitacionesComponent } from './components/habitaciones/habitaciones.co
 import { HuespedesComponent } from './components/huespedes/huespedes.component';
 import { ReservasComponent } from './components/reservas/reservas.component';
 
- const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full'},
-  { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard],
-    children:[
-    { path: 'habitaciones', component: HabitacionesComponent,canActivate: [AuthGuard]},
-    { path: 'huespedes', component: HuespedesComponent,canActivate: [AuthGuard]},
-    { path: 'reservas', component: ReservasComponent,canActivate: [AuthGuard]},
-    { path: 'usuarios', component: UsuariosComponent,canActivate: [AuthGuard], data: {roles: [Roles.ADMIN]}}
-]},
-  { path: '**', redirectTo: 'dashboard'}
-]; 
+//  const routes: Routes = [
+//   { path: '', redirectTo: '/login', pathMatch: 'full'},
+//   { path: 'login', component: LoginComponent},
+//   { path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard],
+//     children:[
+//     { path: 'habitaciones', component: HabitacionesComponent,canActivate: [AuthGuard]},
+//     { path: 'huespedes', component: HuespedesComponent,canActivate: [AuthGuard]},
+//     { path: 'reservas', component: ReservasComponent,canActivate: [AuthGuard]},
+//     { path: 'usuarios', component: UsuariosComponent,canActivate: [AuthGuard], data: {roles: [Roles.ADMIN]}}
+// ]},
+//   { path: '**', redirectTo: 'dashboard'}
+// ]; 
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+  // acceso directo sin login
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent,
+    children: [
+      { path: 'habitaciones', component: HabitacionesComponent },
+      { path: 'huespedes', component: HuespedesComponent },
+      { path: 'reservas', component: ReservasComponent },
+      { path: 'usuarios', component: UsuariosComponent }
+    ]
+  },
+
+  // acceso directo a componentes individuales (opcional)
+  { path: 'habitaciones', component: HabitacionesComponent },
+  { path: 'huespedes', component: HuespedesComponent },
+  { path: 'reservas', component: ReservasComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+
+  { path: '**', redirectTo: 'dashboard' }
+];
 
 
 @NgModule({
